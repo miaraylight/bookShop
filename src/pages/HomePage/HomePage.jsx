7f48fed03f73276fd19d2809b38549313ee37a28
miaraylight <miaraylight@gmail.com>
2023-03-25 02:52:18 +0100

    written HomePage with css

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,6 +3,40 @@ import s from './style.module.css'
 
 export default function HomePage() {
   return (
-    <div>Home</div>
+    <div className={s.wrapper}>
+        <div className={s.main}>
+            <div className={s.title}>
+                <span> <img src="/media/dash.svg" alt="" /> Welcome To Pages!!!</span>
+                <h1>Your Books From The Best Writer.</h1>
+                <p>We believe that reading books are essential to a healthy culture. They’re where authors can connect with readers.</p>
+            </div>
+            <div className={s.order}>
+                <button>Order Today</button>
+                <button>Read Free Demo</button>
+            </div>
+            <div className={s.info}>
+                <div>
+                    <img src="/media/yellow-dot-icon.png" alt="yellow-dot-icon"/>
+                    <span> Pages: </span>
+                </div>
+                
+                <div>
+                    <img src="/media/yellow-dot-icon.png" alt="yellow-dot-icon"/>
+                    <span> Length: </span>
+                </div>
+                
+                <div>
+                    <img src="/media/yellow-dot-icon.png" alt="yellow-dot-icon"/>
+                    <span> Rating: </span>
+                </div>
+                <p>250 pages</p>
+                <p>10 hours</p>
+                <p>4.5/5 (305 ratings)</p>
+            </div>
+        </div>
+        <div className={s.book}>
+            <img src="/media/book1.png" alt="book-title" />
+        </div>
+    </div>
   )
 }
