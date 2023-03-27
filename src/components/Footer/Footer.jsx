849f7165f25905d528d57452448621fc8a3b9a69
miaraylight <miaraylight@gmail.com>
2023-03-27 13:16:56 +0200

    checked

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ export default function Footer() {
         <div className={s.head}>
             <div className={s.logo}>
                 <img src="../media/logo.svg" alt="logo" />
-                <img src="../public/media/pages.svg" alt="logo1" />
+                <img src="../media/pages.svg" alt="logo1" />
             </div>
             <div className={s['social-media']}>
                 <a className={s['social-media-link']} href="https://www.facebook.com/" target="_blank"><img src="../../../public/media/fb-icon-white.svg" alt="fb-icon" /></a>
